9f8fa8133a37d8bd2866866f4c2b4e44703f6ea5
Paul Hey <paulhey@gmail.com>
2020-03-01 20:08:45 -0800

    Added test cases. Does not account for disconnected.

--- Remove-IdleUsers.ps1
+++ Remove-IdleUsers.ps1
@@ -1,6 +1,11 @@
 # First, get the users from quser
- $quser = quser 2>$null
- $header = ($quser[0] -split '\s\s+').Trim()
- $users = $quser[1..$($quser.Count)] -replace '\s\s+',';' | ConvertFrom-String -Delimiter ';' -PropertyNames $header
- 
- 
+$quser = quser 2>$null
+$header = ($quser[0] -split '\s\s+').Trim()
+$users = $quser[1..$($quser.Count)] -replace '\s\s+', ';' | ConvertFrom-String -Delimiter ';' -PropertyNames $header
+foreach ($user in $users){
+  if ($user.STATE -ne 'ACTIVE') {
+    Write-Host -ForegroundColor Green "[State : $($user.STATE)] logoff $($user.ID)"
+  } else {
+    Write-Host -ForegroundColor Magenta "[State : $($user.STATE)] User $($user.USERNAME) is $($user.STATE)."
+  }
+}
