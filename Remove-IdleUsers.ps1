100fbf11eac6dc1eb19ff84745af51804fa6c04c
Paul Hey <paulhey@gmail.com>
2020-03-01 21:34:55 -0800

    Fixed formatting

--- Remove-IdleUsers.ps1
+++ Remove-IdleUsers.ps1
@@ -5,12 +5,12 @@ $quser = query user 2>$null
 $header = ($quser[0] -split '\s\s+').Trim()
 $users = $quser[1..$($quser.Count)] 
 foreach ($line in $users){
-    if($line -match 'Disc'){
-        # need to replace empty session name with something. -- in this case
-        $line = $line -replace "^(\s\w+\s\s)",'$1--'
-    }
-    $user = $line -replace '\s\s+', ';' |
-        ConvertFrom-String -Delimiter ';' -PropertyNames $header
+  if ($line -match 'Disc'){
+    # need to replace empty session name with something. -- in this case
+    $line = $line -replace "^(\s\w+\s\s)", '$1--'
+  }
+  $user = $line -replace '\s\s+', ';' |
+    ConvertFrom-String -Delimiter ';' -PropertyNames $header
 
   if ($user.STATE -ne 'ACTIVE') {
     Write-Host -ForegroundColor Green "[State : $($user.USERNAME) : $($user.STATE)] logoff $($user.ID)"
