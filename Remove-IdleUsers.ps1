34b416b549a4a6aa25ad34f37c144eb4f487f3be
Paul Hey <paulhey@gmail.com>
2020-03-01 20:51:59 -0800

    Updated to actually work

--- Remove-IdleUsers.ps1
+++ Remove-IdleUsers.ps1
@@ -1,11 +1,21 @@
+Start-Transcript -Path $PSScriptRoot\RemoveIdleUsers.log -Append
+
 # First, get the users from quser
-$quser = quser 2>$null
+$quser = query user 2>$null
 $header = ($quser[0] -split '\s\s+').Trim()
-$users = $quser[1..$($quser.Count)] -replace '\s\s+', ';' | ConvertFrom-String -Delimiter ';' -PropertyNames $header
-foreach ($user in $users){
+$users = $quser[1..$($quser.Count)] 
+foreach ($line in $users){
+    if($line -match 'Disc'){
+        # need to replace empty session name with something. -- in this case
+        $line = $line -replace "^(\s\w+\s\s)",'$1--'
+    }
+    $user = $line -replace '\s\s+', ';' |
+        ConvertFrom-String -Delimiter ';' -PropertyNames $header
+
   if ($user.STATE -ne 'ACTIVE') {
-    Write-Host -ForegroundColor Green "[State : $($user.STATE)] logoff $($user.ID)"
+    Write-Host -ForegroundColor Green "[State : $($user.USERNAME) : $($user.STATE)] logoff $($user.ID)"
+    logoff $user.ID
   } else {
-    Write-Host -ForegroundColor Magenta "[State : $($user.STATE)] User $($user.USERNAME) is $($user.STATE)."
+    Write-Host -ForegroundColor Magenta "[State : $($user.USERNAME) : $($user.STATE)] User is $($user.STATE)."
   }
 }
