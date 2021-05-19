7d927a25ad35d7fd13fe1e822f55149f094fbf17
Paul Hey <paulhey@gmail.com>
2021-05-19 13:50:13 -0700

    Added hashtable demo

--- Remove-IdleUsers.ps1
+++ Remove-IdleUsers.ps1
@@ -3,11 +3,11 @@ Start-Transcript -Path $PSScriptRoot\RemoveIdleUsers.log -Append
 # First, get the users from quser
 $quser = query user 2>$null
 $header = ($quser[0] -split '\s\s+').Trim()
-$users = $quser[1..$($quser.Count)] 
+$users = $quser[1..$($quser.Count)]
 foreach ($line in $users){
   if ($line -match 'Disc'){
     # need to replace empty session name with something. -- in this case
-    $line = $line -replace "^(\s\w+\s\s)", '$1--'
+    $line = $line -replace '^(\s\w+\s\s)', '$1--'
   }
   $user = $line -replace '\s\s+', ';' |
     ConvertFrom-String -Delimiter ';' -PropertyNames $header
