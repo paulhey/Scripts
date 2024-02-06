31b5665441090945e8884500a883c73ad0878aaf
Paul Hey <paulhey@gmail.com>
2024-02-05 19:09:06 -0800

    Applied formatting

--- Remove-IdleUsers.ps1
+++ Remove-IdleUsers.ps1
@@ -6,20 +6,20 @@ Write-Host "-- QUSER Start --"
 Write-Host $quser
 Write-Host "-- QUSER End   --"
 
-if([string]::IsNullOrEmpty($quser.trim())) {
+if ([string]::IsNullOrEmpty($quser.trim())) {
   Write-Host "No users to logoff"
   exit
 } else {
   # Clean up headers
   $header = ($quser[0] -split '\s\s+').Trim()
-  $header = $header -replace ' ',''
+  $header = $header -replace ' ', ''
 
   # Split the text into the user sessions
   $users = $quser[1..$($quser.Count)] 
-  foreach ($line in $users){
-    if($line -match 'Disc'){
+  foreach ($line in $users) {
+    if ($line -match 'Disc') {
       # need to replace empty session name with something. -- in this case
-      $line = $line -replace "^(\s[\w\-]+\s\s)",'$1--'
+      $line = $line -replace "^(\s[\w\-]+\s\s)", '$1--'
     }
 
     $user = $line -replace '\s\s+', ';' |
