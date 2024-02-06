e871c909841595dab3341121e7c728c0f42d552b
Paul Hey <paulhey@gmail.com>
2024-02-05 17:06:08 -0800

    Updated script: - addressed empty quser output - ignored log file

--- Remove-IdleUsers.ps1
+++ Remove-IdleUsers.ps1
@@ -1,21 +1,35 @@
 Start-Transcript -Path $PSScriptRoot\RemoveIdleUsers.log -Append
 
 # First, get the users from quser
-$quser = query user 2>$null
-$header = ($quser[0] -split '\s\s+').Trim()
-$users = $quser[1..$($quser.Count)] 
-foreach ($line in $users){
-    if($line -match 'Disc'){
-        # need to replace empty session name with something. -- in this case
-        $line = $line -replace "^(\s\w+\s\s)",'$1--'
-    }
-    $user = $line -replace '\s\s+', ';' |
-        ConvertFrom-String -Delimiter ';' -PropertyNames $header
+$quser = (query user 2>$null)
+Write-Host "-- QUSER Start --"
+Write-Host $quser
+Write-Host "-- QUSER End   --"
 
-  if ($user.STATE -ne 'ACTIVE') {
-    Write-Host -ForegroundColor Green "[State : $($user.USERNAME) : $($user.STATE)] logoff $($user.ID)"
-    logoff $user.ID
-  } else {
-    Write-Host -ForegroundColor Magenta "[State : $($user.USERNAME) : $($user.STATE)] User is $($user.STATE)."
+if([string]::IsNullOrEmpty($quser.trim())) {
+  Write-Host "No users to logoff"
+  exit
+} else {
+  # Clean up headers
+  $header = ($quser[0] -split '\s\s+').Trim()
+  $header = $header -replace ' ',''
+
+  # Split the text into the user sessions
+  $users = $quser[1..$($quser.Count)] 
+  foreach ($line in $users){
+    if($line -match 'Disc'){
+      # need to replace empty session name with something. -- in this case
+      $line = $line -replace "^(\s[\w\-]+\s\s)",'$1--'
+    }
+
+    $user = $line -replace '\s\s+', ';' |
+      ConvertFrom-String -Delimiter ';' -PropertyNames $header
+
+    if ($user.STATE -ne 'ACTIVE') {
+      Write-Host -ForegroundColor Green "[State : $($user.USERNAME) : $($user.STATE)] logoff $($user.ID)"
+      logoff $user.ID
+    } else {
+      Write-Host -ForegroundColor Magenta "[State : $($user.USERNAME) : $($user.STATE)] User is $($user.STATE)."
+    }
   }
 }
